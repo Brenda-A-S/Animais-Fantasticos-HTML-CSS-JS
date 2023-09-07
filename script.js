077a5135b46691e2d5a1bfdf5d6d6606812c45cb
Brenda-A-S <brenda.antunes16@gmail.com>
2023-09-07 19:16:43 -0300

    feat: add function animação ao scroll

--- script.js
+++ script.js
@@ -22,21 +22,44 @@ function initTabNav() {
 initTabNav()
 
 function initAccordion() {
-    const accordionList = document.querySelectorAll('.js-accordion dt');
-    const activeClass = 'ativo';
+    const accordionList = document.querySelectorAll('.js-accordion dt')
+    const activeClass = 'ativo'
 
     if (accordionList.length) {
-        accordionList[0].classList.add(activeClass);
-        accordionList[0].nextElementSibling.classList.add(activeClass);
+        accordionList[0].classList.add(activeClass)
+        accordionList[0].nextElementSibling.classList.add(activeClass)
 
         function activeAccordion() {
-            this.classList.toggle(activeClass);
-            this.nextElementSibling.classList.toggle(activeClass);
+            this.classList.toggle(activeClass)
+            this.nextElementSibling.classList.toggle(activeClass)
         }
 
         accordionList.forEach((item) => {
-            item.addEventListener('click', activeAccordion);
-        });
+            item.addEventListener('click', activeAccordion)
+        })
     }
 }
-initAccordion();
+initAccordion()
+
+function initAnimacaoScroll() {
+    const sections = document.querySelectorAll('.js-scroll')
+    if (sections.length) {
+        const windowMetade = window.innerHeight * 0.6
+
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+                if (isSectionVisible)
+                    section.classList.add('ativo')
+                else
+                    section.classList.remove('ativo')
+            })
+        }
+
+        animaScroll();
+
+        window.addEventListener('scroll', animaScroll)
+    }
+}
+initAnimacaoScroll()
