41d324bed4df9d95438de7924ece9564b6cf51ca
Brenda-A-S <brenda.antunes16@gmail.com>
2023-09-29 18:30:51 -0300

    feat: add menu dropdown

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimacaoScroll from './modules/scroll.js'
 import initAccordion from './modules/accordion.js'
 import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
+import initDropdownMenu from './modules/dropdownMenu.js'
 
 initTabNav()
 initAnimacaoScroll()
 initAccordion()
 initModal()
 initTooltip()
+initDropdownMenu()
