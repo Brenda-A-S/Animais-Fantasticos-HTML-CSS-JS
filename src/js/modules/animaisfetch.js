a23b075c2474f4439c562e3cde29dc7926b6b32b
Brenda-A-S <brenda.antunes16@gmail.com>
2023-11-15 19:58:49 -0300

    refactor: refatoração de fetch

--- src/js/modules/animaisfetch.js
+++ src/js/modules/animaisfetch.js
@@ -1,24 +1,34 @@
-import AnimaNumeros from './animanumeros.js'
+import AnimaNumeros from './animanumeros.js';
 
-export default function initAnimaisFetch() {
-    async function fetchAnimais(url) {
-        const animaisResponse = await fetch(url)
-        const animaisJson = await animaisResponse.json()
-        const numerosGrid = document.querySelector('.numeros-grid')
-        animaisJson.forEach(animal => {
-            const divAnimal = createAnimal(animal)
-            numerosGrid.appendChild(divAnimal)
-        })
-        const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
-        animaNumeros.init();
+export default function fetchAnimais(url, target) {
+  function createAnimal(animal) {
+    const div = document.createElement('div');
+    div.classList.add('numero-animal');
+    div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
+    return div;
+  }
+
+  const numerosGrid = document.querySelector(target);
+  function preencherAnimais(animal) {
+    const divAnimal = createAnimal(animal);
+    numerosGrid.appendChild(divAnimal);
+  }
+
+  function animaAnimaisNumeros() {
+    const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');
+    animaNumeros.init();
+  }
+
+  async function criarAnimais() {
+    try {
+      const animaisResponse = await fetch(url);
+      const animaisJSON = await animaisResponse.json();
+      animaisJSON.forEach(animal => preencherAnimais(animal));
+      animaAnimaisNumeros();
+    } catch (erro) {
+      console.log(erro);
     }
+  }
 
-    function createAnimal(animal) {
-        const div = document.createElement('div')
-        div.classList.add('numero-animal')
-        div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`
-        return div
-    }
-
-    fetchAnimais('./animaisapi.json')
+  return criarAnimais();
 }
